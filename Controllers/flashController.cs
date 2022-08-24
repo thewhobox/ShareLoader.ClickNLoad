de6974079783b7cb7c1bf4db9f90ba1263fc3aec
thewhobox <45664417+thewhobox@users.noreply.github.com>
2022-08-24 15:18:34 +0200

    replace RijndaelManaged

--- Controllers/flashController.cs
+++ Controllers/flashController.cs
@@ -29,25 +29,52 @@ public class flashController : Controller
 
         byte[] dataByte = Convert.FromBase64String(crypted);
 
-        RijndaelManaged rDel = new RijndaelManaged();
-        System.Text.ASCIIEncoding aEc = new System.Text.ASCIIEncoding();
+        //RijndaelManaged rDel = new RijndaelManaged();
+        //System.Text.ASCIIEncoding aEc = new System.Text.ASCIIEncoding();
 
         jk = jk.ToUpper();
-        String decKey = "";
+        string decKey = "";
         for (int i = 0; i < jk.Length; i += 2)
         {
             decKey += (char)Convert.ToUInt16(jk.Substring(i, 2), 16);
         }
 
-        rDel.Key = aEc.GetBytes(decKey);
+        /*rDel.Key = aEc.GetBytes(decKey);
         rDel.IV = aEc.GetBytes(decKey);
         rDel.Mode = CipherMode.CBC;
 
         rDel.Padding = PaddingMode.None;
         ICryptoTransform cTransform = rDel.CreateDecryptor();
         byte[] resultArray = cTransform.TransformFinalBlock(dataByte, 0, dataByte.Length);
+        string rawLinks = aEc.GetString(resultArray);*/
+        string rawLinks = "";
 
-        string rawLinks = aEc.GetString(resultArray);
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = System.Text.Encoding.ASCII.GetBytes(decKey);
+            aesAlg.IV = System.Text.Encoding.ASCII.GetBytes(decKey);
+            aesAlg.Padding = PaddingMode.None;
+            aesAlg.Mode = CipherMode.CBC;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(dataByte))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        rawLinks = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
         Regex rgx = new Regex("\u0000+$");
         rawLinks = rgx.Replace(rawLinks, "");
         rgx = new Regex("\n+");
